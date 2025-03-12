2b167137f52fb3681b1e201eb2e0b5149621f68f
Fakenhit <mutiev20@yandex.ru>
2025-03-13 02:09:32 +0300

    доработка функционала

--- listMusic.js
+++ listMusic.js
@@ -52,3 +52,33 @@ const Radio_Los_Santos = [
         src: ""
     },
 ];
+
+
+const Space = [
+    {
+        audioName: "Space",
+        src: `audio/GTAV/Radio Los Santos/Jay Rock feat. Kendrick Lamar — Hood Gone Love It (2011).mp3`
+    },
+    {
+        audioName: "Space2",
+        src: "primer.mp3"
+    }];
+
+const West_Coast_Classics = [
+    {
+        audioName: "West Coast Classics",
+        src: `audio/GTAV/Radio Los Santos/Jay Rock feat. Kendrick Lamar — Hood Gone Love It (2011).mp3`
+    },
+    {
+        audioName: "West Coast Classics2",
+        src: "primer.mp3"
+    }];
+const Rebel_Radio = [
+    {
+        audioName: "Rebel Radio",
+        src: `audio/GTAV/Radio Los Santos/Jay Rock feat. Kendrick Lamar — Hood Gone Love It (2011).mp3`
+    },
+    {
+        audioName: "Rebel Radio2",
+        src: "primer.mp3"
+    }];
--- player.js
+++ player.js
@@ -1,10 +1,27 @@
 
 
 
-
+const KeyPlay = document.getElementById("playstop");
 const audioPlayer =  document.getElementById("audioPlayer");
 const PlayerController = document.getElementById("playerControler");
 const playerControlerName = document.getElementById("controllerName");
+const timePicker = document.getElementById("timePicker");
+
+audioPlayer.addEventListener('timeupdate', function () {
+    timePicker.innerHTML = secondsToTime(audioPlayer.currentTime);
+}, false);
+
+function PlayStop() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        KeyPlay.src = "images/pause.svg";
+
+        
+    } else {
+        audioPlayer.pause();
+        KeyPlay.src = "images/Play.svg";
+    }
+}
 
 function Bit(src, name) {
     console.log("Клик по поесни");
@@ -14,30 +31,93 @@ function Bit(src, name) {
         audioPlayer.src = src;
         playerControlerName.innerText = name; 
     }
-    
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-    } else {
-        audioPlayer.pause();
-    }
+    PlayStop();
+
     PlayerController.classList.remove("d-none");
 
 }
 
 
+KeyPlay.addEventListener("click", function() {
+    PlayStop();
+});
 
-for (let mk of Radio_Los_Santos) {
-    let div = document.createElement('div');
-    div.className = "row mt-1";
-    div.addEventListener("click", function() {
-        Bit(mk.src, mk.audioName);
+function removeClassFromAll() {
+    // Выбираем все элементы с классом "example"
+    const elements = document.querySelectorAll('.active');
+  
+    // Проходимся по каждому элементу и удаляем класс
+    elements.forEach(element => {
+      element.classList.remove('active'); // Удаляем класс
     });
-    div.innerHTML = `<div class="col-12 composition mx-5"><a>${mk.audioName}</div>`;
-    
-    let music = document.getElementById('music');
-    music.append(div);
+  }
+
+
+function list(radio) {
+    const music = document.getElementById("music");
+    while(music.firstChild){
+        music.removeChild(music.firstChild);
+    }
+    for (let mk of radio) {
+        let div = document.createElement('div');
+        div.className = "row mt-1";
+        div.addEventListener("click", function() {
+            Bit(mk.src, mk.audioName);
+            removeClassFromAll();
+            div.classList.add("active");
+        });
+        div.innerHTML = `<div class="col-12 composition hov hover mx-5"><a>${mk.audioName}</div>`;
+        
+        let music = document.getElementById('music');
+        music.append(div);
+    }
 }
 
+list(West_Coast_Classics);
+
+audioPlayer.addEventListener('loadedmetadata', function() {
+    const duration = audioPlayer.duration; // Длительность в секундах
+    const formattedDuration = formatTime(duration); // Форматируем время
+    document.getElementById('duration').textContent = formattedDuration;
+});
+
+// Функция для преобразования секунд в минуты и секунды
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60); // Получаем минуты
+    const secs = Math.floor(seconds % 60); // Получаем секунды
+    // Добавляем ведущий ноль, если секунд меньше 10
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+
+function secondsToTime(time){
+             
+    var h = Math.floor(time / (60 * 60)),
+        dm = time % (60 * 60),
+        m = Math.floor(dm / 60),
+        ds = dm % 60,
+        s = Math.ceil(ds);
+    if (s === 60) {
+        s = 0;
+        m = m + 1;
+    }
+    if (s < 10) {
+        s = '0' + s;
+    }
+    if (m === 60) {
+        m = 0;
+        h = h + 1;
+    }
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (h === 0) {
+        fulltime = m + ':' + s;
+    } else {
+        fulltime = h + ':' + m + ':' + s;
+    }
+    return fulltime;
+}
 
 
 
